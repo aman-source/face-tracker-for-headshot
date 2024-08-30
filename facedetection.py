26e3a8f03bf7c8aed01c2dcded986dd041d0307c
aman <61686116+aman-source@users.noreply.github.com>
2024-08-30 11:39:01 +0530

    face tracker

--- facedetection.py
+++ facedetection.py
@@ -2,36 +2,39 @@ import cv2
 import numpy as np
 import time
 
-pengklasifikasiWajah  = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# Load the face detection classifier
+face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-videoCam = cv2.VideoCapture(0)
+# Open the webcam
+video_capture = cv2.VideoCapture(0)
 
-if not videoCam.isOpened():
-    print("Kamera tidak dapat diakses")
+if not video_capture.isOpened():
+    print("Camera could not be accessed")
     exit()
 
-tombolQditekan = False
-while (tombolQditekan == False):
-    ret, kerangka = videoCam.read()
+# Variable to check if 'q' key is pressed
+q_key_pressed = False
+while not q_key_pressed:
+    ret, frame = video_capture.read()
 
-    if ret == True:
-        abuAbu = cv2.cvtColor(kerangka, cv2.COLOR_BGR2GRAY)
-        dafWajah = pengklasifikasiWajah.detectMultiScale(abuAbu, scaleFactor = 1.3, minNeighbors = 2)
+    if ret:
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=2)
 
-        for (x, y, w, h) in dafWajah:
-            cv2.rectangle(kerangka, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
-        #print("Jumlah Wajah terdeksi: ", len(dafWajah))
-        teks = "Jumlah Wajah Terdeteksi = " + str(len(dafWajah))
+        # Display the number of detected faces
+        text = "Number of Faces Detected = " + str(len(faces))
 
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(kerangka, teks, (0, 30), font, 1, (255, 0, 0), 1)
+        cv2.putText(frame, text, (0, 30), font, 1, (255, 0, 0), 1)
 
-        cv2.imshow("Hasil", kerangka)
+        cv2.imshow("Result", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            tombolQditekan = True
+            q_key_pressed = True
             break
 
-
-videoCam.release()
+# Release the camera and close windows
+video_capture.release()
 cv2.destroyAllWindows()
